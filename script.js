725b186dbf08ea145ec0e697b06df98500511261
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-04-08 17:18:04 +0200

    adding functionality on second click on chart bars

--- script.js
+++ script.js
@@ -12,47 +12,112 @@
                 $('#Wenday').append(amountWed);
             })
 
+            let isAmountMonAdded = false;
+
             $("#mon").click(function() {
-                let amountMon = $('<span>');
-                amountMon.addClass('dayBalanceAmountMon')
-                amountMon.text(`$${response[0].amount}`)
-                $('#Monday').append(amountMon);
+                if(isAmountMonAdded) {
+                    $('.dayBalanceAmountMon').remove();
+                    isAmountMonAdded = false;
+                } else {
+                    let amountMon = $('<span>');
+                    amountMon.addClass('dayBalanceAmountMon')
+                    amountMon.text(`$${response[0].amount}`)
+                    $('#Monday').append(amountMon);
+                    isAmountMonAdded = true;
+                }
             })
+
+            let isAmountTueAdded = false;
 
             $("#tue").click(function() {
-                let amountTue = $('<span>');
-                amountTue.addClass('dayBalanceAmountTue')
-                amountTue.text(`$${response[1].amount}`)
-                $('#Tuesday').append(amountTue);
+                if(isAmountTueAdded) {
+                    $('.dayBalanceAmountTue').remove();
+                    isAmountTueAdded = false;
+                } else {
+                    let amountTue = $('<span>');
+                    amountTue.addClass('dayBalanceAmountTue')
+                    amountTue.text(`$${response[1].amount}`)
+                    $('#Tuesday').append(amountTue);
+                    isAmountTueAdded = true;
+                }
             })
+
+            let isAmountWedAdded = false;
+
+            $("#wed").click(function() {
+                if(isAmountWedAdded) {
+                    $('.dayBalanceAmount').remove();
+                    isAmountWedAdded = false;
+                } else {
+                    let amountWed = $('<span>');
+                    amountWed.addClass('dayBalanceAmount')
+                    amountWed.text(`$${response[2].amount}`)
+                    $('#Wenday').append(amountWed);
+                    isAmountWedAdded = true;
+                }
+            })
+
+            let isAmountThuAdded = false;
 
             $("#thu").click(function() {
-                let amountThu = $('<span>');
-                amountThu.addClass('dayBalanceAmountThu')
-                amountThu.text(`$${response[3].amount}`)
-                $('#Thursday').append(amountThu);
+                if(isAmountThuAdded) {
+                    $('.dayBalanceAmountThu').remove();
+                    isAmountThuAdded = false;
+                } else {
+                    let amountThu = $('<span>');
+                    amountThu.addClass('dayBalanceAmountThu')
+                    amountThu.text(`$${response[3].amount}`)
+                    $('#Thursday').append(amountThu);
+                    isAmountThuAdded = true;
+                }
             })
+
+            let isAmountFriAdded = false;
 
             $("#fri").click(function() {
-                let amountFri = $('<span>');
-                amountFri.addClass('dayBalanceAmountFri')
-                amountFri.text(`$${response[4].amount}`)
-                $('#Friday').append(amountFri);
+                if(isAmountFriAdded) {
+                    $('.dayBalanceAmountFri').remove();
+                    isAmountFriAdded = false;
+                } else {
+                    let amountFri = $('<span>');
+                    amountFri.addClass('dayBalanceAmountFri')
+                    amountFri.text(`$${response[4].amount}`)
+                    $('#Friday').append(amountFri);
+                    isAmountFriAdded = true;
+                }
             })
+
+            let isAmountSatAdded = false;
 
             $("#sat").click(function() {
-                let amountSat = $('<span>');
-                amountSat.addClass('dayBalanceAmountSat')
-                amountSat.text(`$${response[5].amount}`)
-                $('#Satrday').append(amountSat);
+                if(isAmountSatAdded) {
+                    $('.dayBalanceAmountSat').remove();
+                    isAmountSatAdded = false;
+                } else {
+                    let amountSat = $('<span>');
+                    amountSat.addClass('dayBalanceAmountSat')
+                    amountSat.text(`$${response[5].amount}`)
+                    $('#Satrday').append(amountSat);
+                    isAmountSatAdded = true;
+                }
             })
 
+            let isAmountSunAdded = false;
+
             $("#sun").click(function() {
-                let amountSun = $('<span>');
-                amountSun.addClass('dayBalanceAmountSun')
-                amountSun.text(`$${response[6].amount}`)
-                $('#Sunday').append(amountSun);
+                if(isAmountSunAdded) {
+                    $('.dayBalanceAmountSun').remove();
+                    isAmountSunAdded = false;
+                } else {
+                    let amountSun = $('<span>');
+                    amountSun.addClass('dayBalanceAmountSun')
+                    amountSun.text(`$${response[6].amount}`)
+                    $('#Sunday').append(amountSun);
+                    isAmountSunAdded = true;
+                }
             })
+
+            
         }
     })
 
